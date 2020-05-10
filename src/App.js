bb61eae1cdde75e6fa678980b70c5c24eae6f55d
VaibhavAvasarala <krishna_vaibhav@yahoo.co.in>
2020-05-10 13:58:47 +0530

    Added Sign-In related components,pages

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import './App.css';
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 const HatsPage = () => (<div><h1>Hats Page</h1></div>);
 
 function App() {
   return (
     <div >
-      {/* <HomePage></HomePage> */}
       <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/hats' component={HatsPage} />
         <Route exact path="/shop" component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
